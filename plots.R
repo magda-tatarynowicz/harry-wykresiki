e91f26cf113619786ea179ab4451536081b5c134
Magda Tatarynowicz <tatarynowiczm@student.mini.pw.edu.pl>
2017-12-05 23:31:15 +0100

    Presentation

--- plots.R
+++ plots.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(plotly)
 library(dplyr)
 library(rCharts)
+library(scales)
 
 library(tidytext)
 data(stop_words)
@@ -31,26 +32,58 @@ p1
 ggplotly(p1)
 
 
-process_sentiment <- function(set, id){
+process_sentiment <- function(set, id, scale_max){
   sentiment <- set %>% inner_join(get_sentiments("afinn"))
-  data.frame(score = cumsum(sentiment$score), index = seq_along(sentiment$score), id = id)
+  df <- data.frame(score = cumsum(sentiment$score), index = seq_along(sentiment$score), Part = id, word = sentiment$word)
+  df["index_scaled"] <- rescale(df$index, to = c(0, scale_max))
+  df
 }
 
-sentiment1 <- process_sentiment(part1, "1")
-sentiment2 <- process_sentiment(part2, "2")
-sentiment3 <- process_sentiment(part3, "3")
-sentiment4 <- process_sentiment(part4, "4")
-sentiment5 <- process_sentiment(part5, "5")
-sentiment6 <- process_sentiment(part6, "6")
-sentiment7 <- process_sentiment(part7, "7")
-sentiment8 <- process_sentiment(part8, "8")
+movie_scale <- 800
+sentiment1 <- process_sentiment(part1, "1", movie_scale)
+sentiment2 <- process_sentiment(part2, "2", movie_scale)
+sentiment3 <- process_sentiment(part3, "3", movie_scale)
+sentiment4 <- process_sentiment(part4, "4", movie_scale)
+sentiment5 <- process_sentiment(part5, "5", movie_scale)
+sentiment6 <- process_sentiment(part6, "6", movie_scale)
+sentiment7 <- process_sentiment(part7, "7", movie_scale)
+sentiment8 <- process_sentiment(part8, "8", movie_scale)
 
 sentiment <- rbind(sentiment1, sentiment2, sentiment3, sentiment4,
                    sentiment5, sentiment6, sentiment7, sentiment8)
 
-p2 <- ggplot() + geom_line(aes(y = score, x = index, color = id), data = sentiment)
-p2
-ggplotly(p2)
+p2_movie <- ggplot() + geom_line(aes(y = score, x = index_scaled, color = Part, label = word), data = sentiment) +
+  scale_colour_brewer(palette = "Paired") + 
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.ticks.y=element_blank())
+p2_movie
+ggplotly(p2_movie, tooltip = c("Part", "word"))
+
+book_scale <- 10000
+sentiment_book1 <- process_sentiment(book1, "1", book_scale)
+sentiment_book2 <- process_sentiment(book2, "2", book_scale)
+sentiment_book3 <- process_sentiment(book3, "3", book_scale)
+sentiment_book4 <- process_sentiment(book4, "4", book_scale)
+sentiment_book5 <- process_sentiment(book5, "5", book_scale)
+sentiment_book6 <- process_sentiment(book6, "6", book_scale)
+sentiment_book7 <- process_sentiment(book7, "7", book_scale)
+
+sentiment_book <- rbind(sentiment_book1, sentiment_book2, sentiment_book3, sentiment_book4,
+                        sentiment_book5, sentiment_book6, sentiment_book7)
+
+p2_book <- ggplot() + geom_line(aes(y = score, x = index_scaled, color = Part, label = word), data = sentiment_book) + 
+  scale_colour_brewer(palette = "Paired") + 
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.ticks.y=element_blank())
+p2_book
+ggplotly(p2_book, tooltip = c("Part", "word"))
+
 
 count_negative_words <- function(set, negative, partNum) {
   res <- table(as.character(set$word[set$word %in% negative]))
