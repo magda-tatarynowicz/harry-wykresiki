30705350a3dcfc3fb7e7be3bb519d5f7b1937835
Magda Tatarynowicz <tatarynowiczm@student.mini.pw.edu.pl>
2017-12-17 20:24:27 +0100

    Small changes in layout

--- ui.R
+++ ui.R
@@ -3,21 +3,22 @@ require(rCharts)
 
 spells <- read.csv("spells_frame.csv")[,1]
 
-
-
 shinyUI(fluidPage(
   
-  titlePanel("Harry Potter"),
+  h1("Harry Potter", align="center"),
   
-  sidebarLayout(
-    sidebarPanel(
-       selectInput("spells", "Zaklęcia", spells, selected=c("Expelliarmus", "Avada Kedavra"), multiple = TRUE)
-    ),
+  fluidRow(
+    column(12, align="center",
+           selectInput("spells", "Zaklęcia", spells, selected=c("Expelliarmus", "Avada Kedavra"), multiple = TRUE, width = '100%'))
+  ),
     
-    
-    mainPanel(
-      showOutput("spellMoviePlot", "nvd3"),
-      showOutput("spellBookPlot", "nvd3")
+  fluidRow(
+    column(6,
+            h3("Filmy",align="center"),
+            showOutput("spellMoviePlot", "nvd3")),
+    column(6,
+            h3("Książki",align="center"),
+            showOutput("spellBookPlot", "nvd3"))
     )
   )
-))
+)
