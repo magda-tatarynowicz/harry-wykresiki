fbee1ea278398bd545bbfd83793f4db9863d7310
karol-14 <karolprusinowski@gmail.com>
2017-12-20 00:21:30 +0100

    Small layout tweaks

--- ui.R
+++ ui.R
@@ -7,38 +7,39 @@ shinyUI(fluidPage(
   
   h1("Harry Potter", align="center"),
   
-  conditionalPanel(condition = "input.filter == 'custom'",
+  conditionalPanel(
+    condition = "input.activeTab != 'Spells\\' description'",
     fluidRow(
-      column(12, align="center",
-             selectInput("spells", "Spells", spells, selected=c("Expecto Patronum", "Riddikulus"), multiple = TRUE, width = '100%'))
-  )),
-  
-  fluidRow(
-    column(12, align="center", radioButtons("filter", NULL,
-                 c("Custom filter" = "custom",
-                   "Unforgivable Curses" = "forbidden",
-                   "Duelling" = "duels",
-                   "Popular" = "popular"), inline = TRUE))
+      column(12, align="center", radioButtons("filter", NULL,
+                                              c("Custom filter" = "custom",
+                                                "Unforgivable Curses" = "forbidden",
+                                                "Duelling" = "duels",
+                                                "Popular" = "popular"), inline = TRUE))
+    ),
+    
+    conditionalPanel(condition = "input.filter == 'custom'",
+                     fluidRow(
+                       column(12, align="center",
+                              selectInput("spells", "Spells", spells, selected=c("Expecto Patronum", "Riddikulus"), multiple = TRUE, width = '100%'))
+                     ))
   ),
   
+  
   tabsetPanel(
+    id ="activeTab",
     tabPanel("Area chart",
-             fluidRow(
-                 column(6,
-                         h3("Movies",align="center"),
-                         showOutput("spellMoviePlotArea", "nvd3")),
-                 column(6,
-                         h3("Books",align="center"),
-                         showOutput("spellBookPlotArea", "nvd3"))
+             splitLayout(
+                   div(h3("Movies",align="center"),
+                       showOutput("spellMoviePlotArea", "nvd3")),
+                   div(h3("Books",align="center"),
+                       showOutput("spellBookPlotArea", "nvd3"))
                  )),
    tabPanel("Bar chart",
-          fluidRow(
-                 column(6,
-                        h3("Movies",align="center"),
-                        showOutput("spellMoviePlot", "nvd3")),
-                 column(6,
-                        h3("Books",align="center"),
-                        showOutput("spellBookPlot", "nvd3"))
+            splitLayout(
+                 div(h3("Movies",align="center"),
+                     showOutput("spellMoviePlot", "nvd3")),
+                 div(h3("Books",align="center"),
+                     showOutput("spellBookPlot", "nvd3"))
                  )),
    tabPanel("Spells' description",
           dataTableOutput('spellsDescription')))
